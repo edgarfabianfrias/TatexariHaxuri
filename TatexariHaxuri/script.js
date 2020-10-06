f0c6cab3bdcb38d4f783c9c03ed07e020a322029
edgarfabianfrias <70584909+edgarfabianfrias@users.noreply.github.com>
2020-10-06 10:27:56 -0500

    Add files via upload

---
+++ TatexariHaxuri/script.js
@@ -0,0 +1,15 @@
+var a = 5;
+var b = 5;
+var theNumbersMatch
+
+if ( a == b ) {
+    theNumbersMatch = true;
+} else {
+    theNumbersMatch = false;
+}
+
+console.log(theNumbersMatch);
+
+var pens;
+pens = ["orange", "red", "green", "blue"]
+console.log(pens);
