086f898c8921cef9a88d70859140a12a8f9f8f66
edgarfabianfrias <70584909+edgarfabianfrias@users.noreply.github.com>
2020-10-06 10:33:04 -0500

    Update script.js

--- TatexariHaxuri/script.js
+++ TatexariHaxuri/script.js
@@ -1,15 +1,46 @@
-var a = 5;
-var b = 5;
-var theNumbersMatch
+class InterstitialSmuggler{
 
-if ( a == b ) {
-    theNumbersMatch = true;
-} else {
-    theNumbersMatch = false;
-}
+    constructor(){
+      this.name = 'TatexaraHaxuri';
+      this.gender = 'oracular'
+      this.home = 'liminal';
+      this.power = "telekenisis";
+      this.language = "telepathy";
+      this.backgroundSound = "void"; 
+      this.seeking = ['transmutation', 'fire', 'water'];
+      this.path = 'embodiment';
+  
+    }
+  
+    if (planetaryNeed > 0) {
+        result = mutate;
+    }else {
+        result = radicalRespite;
+        }
 
-console.log(theNumbersMatch);
 
-var pens;
-pens = ["orange", "red", "green", "blue"]
-console.log(pens);
+
+   intervene (){
+      //support organisms on earth when threatened
+      if (need arises) {
+        become (embody organism)
+      }
+    }
+
+   possesion(){
+      //once in host, Tatexari Haxuri bestows power and guidance
+      if (embodied) {
+       result = mutant power activate;
+      } else {
+      action = approach;
+       result = mutant power engage;
+      }
+      return result;
+      }
+    }
+    
+    process(){
+      //Tatexari Haxuri supports neurological integration through conversation and care
+       }
+  
+  }
